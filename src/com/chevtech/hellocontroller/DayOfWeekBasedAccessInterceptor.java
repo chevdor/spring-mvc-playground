e90478714d424beb5c66100b9cd0b5560aa6050e
Chevdor <chevdor@gmail.com>
2015-11-01 20:07:05 +0100

    Interceptor config

--- src/com/chevtech/hellocontroller/DayOfWeekBasedAccessInterceptor.java
+++ src/com/chevtech/hellocontroller/DayOfWeekBasedAccessInterceptor.java
@@ -4,25 +4,48 @@ import java.util.Calendar;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.util.SocketUtils;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class DayOfWeekBasedAccessInterceptor extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		
+
 		// true => keep going
 		// false => we stop handling the request
-		
+
 		Calendar cal = Calendar.getInstance();
 		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
-		
+
 		if(dayOfWeek==1){
 			response.getWriter().write("Website closed on sundays, come back later");
-			
+
 			return false;
 		}
-		
+
 		return true;
 	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		//super.postHandle(request, response, handler, modelAndView);
+		System.out.println("HandlerInterceptorAdapter: Spring MVC called postHandle method for "+ 
+				request.getRequestURI().toString());
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		//		super.afterCompletion(request, response, handler, ex);
+		System.out.println("HandlerInterceptorAdapter: Spring MVC called afterCompletion method for "+ 
+				request.getRequestURI().toString());
+
+	}
+
 }
