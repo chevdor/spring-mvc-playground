0dd8118a85cf06a26212821703eabd2e6f1b9b22
Will <wilfried.kopp@topimagesystems.com>
2015-11-01 15:52:10 +0100

    Custom date format

--- src/com/chevtech/hellocontroller/HtmlFormController.java
+++ src/com/chevtech/hellocontroller/HtmlFormController.java
@@ -1,10 +1,18 @@
 package com.chevtech.hellocontroller;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.Map;
 
+import javax.servlet.jsp.tagext.FunctionInfo;
+
+import org.springframework.beans.factory.annotation.CustomAutowireConfigurer;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,13 +22,19 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/form")
 public class HtmlFormController {
-	
+	@InitBinder
+	public void initBinder(WebDataBinder binder){
+		binder.setDisallowedFields(new String[]{"mobile"});
+		SimpleDateFormat funnyFormat= new SimpleDateFormat("yyyy***MM***dd");
+		binder.registerCustomEditor(Date.class, "dob", new CustomDateEditor(funnyFormat,false));
+	}
+
 	@RequestMapping(value="/subscribe", method = RequestMethod.GET)
 	protected ModelAndView subscribe(){ 
 		ModelAndView modelanview = new ModelAndView("Admission");
 		return modelanview;
 	}
-	
+
 	@RequestMapping(value="/confirm", method = RequestMethod.POST)
 	protected ModelAndView confirm(
 			@RequestParam(value="name", defaultValue="n/a") String name,
@@ -28,25 +42,25 @@ public class HtmlFormController {
 			){ 
 		ModelAndView modelanview = new ModelAndView("Confirmation");
 		modelanview.addObject("msg", "Details: " + name + " " + hobby);
-		
+
 		return modelanview;
 	}
 	@ModelAttribute
 	public void addMessage(Model m){
 		m.addAttribute("message", "We play with SpringMVC");
 	}
-	
+
 	@RequestMapping(value="/confirm2", method = RequestMethod.POST)
 	protected ModelAndView confirm2(
 			@ModelAttribute("student") Student student, BindingResult result
 			){ 
-		
+
 		if (result.hasErrors()){
 			// we go back to the initial page
 			ModelAndView modelanview = new ModelAndView("Admission");
 			return modelanview;	
 		}
-		
+
 		ModelAndView modelanview = new ModelAndView("Confirmation2");
 		return modelanview;
 	}
